ba1fa782012a2e610a9cda2ad14118c5a91ec603
Andres cavadia <andrescavadiaaa@gmail.com>
2025-04-18 11:18:53 -0500

    Fix font loading and remove next/font dependency

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+    webpack(config, { isServer }) {
+  
+      if (!isServer) {
+        config.node = {
+          fs: 'empty',
+        };
+      }
+      return config;
+    },
+  };
+  
